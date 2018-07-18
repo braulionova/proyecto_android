424b3eb8236c02ff14be6b5eef76f757ba67a121
Braulio Nova <braulio503@gmail.com>
2018-07-17 23:23:43 -0400

    Creacion de las Activitys ListaTareas, DetalleTarea, RegistroTarea

---
+++ app/src/main/java/com/braulionova/taskapp/vista/ListaTareasActivity.java
@@ -0,0 +1,14 @@
+package com.braulionova.taskapp.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.braulionova.taskapp.R;
+
+public class ListaTareasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_tareas);
+    }
+}
